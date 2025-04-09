8d62e214e53b217330813e3a3f1696499e812f5f
Will Cockram <willcockram@outlook.com>
2025-04-09 17:34:19 +0100

    Update homepage layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = 'Another Dog Blog'
+export const SITE_DESCRIPTION = 'This a personal blog created to document life with my dog Twix. Nervous, anxious, hilarious, beautiful.'
